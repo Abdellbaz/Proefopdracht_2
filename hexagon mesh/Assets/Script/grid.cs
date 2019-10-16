34522b324429899c2ab5f03adb294a8019efa526
Abdellbaz <34577741+Abdellbaz@users.noreply.github.com>
2019-10-16 15:02:42 +0200

    First Push

---
+++ hexagon mesh/Assets/Script/grid.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class grid : MonoBehaviour
+{
+    public GameObject cube;
+    GameObject[,] objs = new GameObject[7,7];
+
+
+    void Start()
+    {
+        
+    
+
+        for (int row = 0; row < objs.GetLength(0); row++)
+        {
+            for (int col = 0; col < objs.GetLength(1); col++)
+            {   if(col % 2 == 0) {
+                objs[row, col] = (GameObject)Instantiate(cube, new Vector3(1.1f * row, 0.85f * col, 0), Quaternion.identity);
+                }
+                else
+                {
+                 objs[row, col] = (GameObject)Instantiate(cube, new Vector3(1.1f * row + 0.55f, 0.85f * col , 0), Quaternion.identity);
+                }
+                objs[row, col].transform.parent = gameObject.transform;
+
+            }
+        }
+
+
+    }
+
+}
---
+++ hexagon mesh/Assets/Script/meshTriangles.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class meshTriangles : MonoBehaviour
+{// Use this for initialization
+    void Start()
+    {
+        gameObject.AddComponent<MeshFilter>();
+        gameObject.AddComponent<MeshRenderer>();
+        Mesh mesh = GetComponent<MeshFilter>().mesh;
+        Vector3 center;
+
+
+        center = new Vector3(0.50f, 0.50f, 0.00f);
+        mesh.vertices = new Vector3[] {
+        center, new Vector3(0.00f, 0.75f, 0.00f), new Vector3(0.50f, 1.00f, 0.00f),
+        center, new Vector3(0.50f, 1.00f, 0.00f), new Vector3(1.00f, 0.75f, 0.00f),
+        center, new Vector3(0.00f, 0.25f, 0.00f), new Vector3(0.00f, 0.75f, 0.00f),
+        center, new Vector3(1.00f, 0.75f, 0.00f), new Vector3(1.00f, 0.25f, 0.00f),
+        center, new Vector3(0.50f, 0.00f, 0.00f), new Vector3(0.00f, 0.25f, 0.00f),
+        center, new Vector3(1.00f, 0.25f, 0.00f), new Vector3(0.50f, 0.00f, 0.00f)};
+
+        mesh.triangles = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17 };
+
+    }
+
+   
+
+    
+}
